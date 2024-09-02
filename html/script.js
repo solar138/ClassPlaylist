4b1be74eac3bfa4b7106483ebbea253b40f8eab1
Solar138 <46548002+solar138@users.noreply.github.com>
2024-09-01 18:35:28 -0700

    lots of changes!

--- html/script.js
+++ html/script.js
@@ -1,7 +1,15 @@
 const errorText = document.getElementById("error");
 
+var settings = {};
+
 if (location.search.includes("error=invalidName")) {
-    errorText.innerText = "Name must be between 1 and 256 alphanumeric characters.";
+  errorText.innerText = "Name must be between 1 and 256 alphanumeric characters.";
+}
+if (location.search.includes("error=wrongUserOrPassword")) {
+    errorText.innerText = "Playlist exists, choose another name or enter correct access password.";
+}
+if (location.search.includes("error=playlistNotFound")) {
+    errorText.innerText = "Playlist does not exist.";
 }
 
 if (location.pathname.startsWith("/playlist/")) {
@@ -17,33 +25,36 @@ if (location.pathname.startsWith("/playlist/")) {
     fetchData().then(x => { 
         songs = x.songs;
         requests = x.requests;
-
-        var content = document.getElementById("content");
-
-        if (songs.length == 0) {
-            content.innerHTML = "<p>No songs in playlist.</p>";
-        }
-        
-        var tag = document.createElement('script');
   
+        var tag = document.createElement('script');
+      
         tag.src = "https://www.youtube.com/iframe_api";
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        var player;
 
-        var requestsList = document.getElementById("requests");
-
-        if (requests.length == 0) {
-            requestsList.innerHTML = "<p>No requests awaiting.</p>";
-        }
-
-        for (let i = 0; i < requests.length; i++) {
-            requestsList.innerHTML += "<p>" + requests[i] + "</p>";
-        }
+        drawSongs();
+        
+        drawRequests();
     });
 }
-function onYouTubeIframeAPIReady() {
-    var currentSong = songs[Math.floor(Math.random() * songs.length)];
+if (location.pathname.startsWith("/request/")) {
+  trackSetting("userName");
+
+  // hide nameRow if name exists
+  if (settings.userName) {
+    document.getElementById("userName").style.display = "none";
+    document.getElementById("nameLabel").innerHTML = settings.userName;
+  }
+}
+var playWhenReady = false;
+function startVideo() {
+
+  if (window.YT == undefined) {
+    playWhenReady = true;
+    return;
+  }
+  var currentSong = songs[Math.floor(Math.random() * songs.length)];
+
   player = new YT.Player('player', {
     height: '390',
     width: '640',
@@ -58,6 +69,13 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
+function onYouTubeIframeAPIReady() {
+  if (playWhenReady) {
+    startVideo();
+  }
+  
+}
+
 // 4. The API will call this function when the video player is ready.
 var done = false;
 function onPlayerReady(event) {
@@ -65,6 +83,7 @@ function onPlayerReady(event) {
     return;
   }
   event.target.playVideo();
+  content.innerHTML = `<p> Playing: ${player.videoTitle}</p>`;
   done = true;
 }
 
@@ -74,15 +93,16 @@ function onPlayerReady(event) {
 function onPlayerStateChange(event) {
   if (event.data == 0) {
     nextVideo();
+  } else {
+    content.innerHTML = `<p> Playing: ${player.videoTitle}</p>`;
   }
 }
 
 function nextVideo() {
-  var currentSong = player.getVideoUrl().slice(-("dQw4w9WgXcQ".length));
-  console.log(player.getVideoUrl());
   if (settings["shuffle"] == "true") {
     nextIndex = Math.floor(Math.random() * songs.length);
   } else {
+    var currentSong = player.getVideoUrl().slice(-("dQw4w9WgXcQ".length));
     nextIndex = songs.indexOf(currentSong) + 1;
     if (nextIndex >= songs.length) {
       nextIndex = 0;
@@ -90,13 +110,30 @@ function nextVideo() {
   }
   var videoId = songs[nextIndex];
   player.loadVideoById(videoId);
+  content.innerHTML = `<p>Loading...</p>`;
 }
 
-var settings = {};
+function removeVideo(index) {
+  if (index !=undefined) {
+    songs.splice(index, 1);
+  } else {
+    var currentSong = player.getVideoUrl().slice(-("dQw4w9WgXcQ".length));
+
+    if (currentSong != undefined && songs.indexOf(currentSong) >= 0) {
+      songs.splice(songs.indexOf(currentSong), 1);
+    }
+  }
+  updatePlaylist();
+  nextVideo();
+}
 
 function trackSetting(id) {
     // add an event listener to track all changes to any input element with the given id.
     var input = document.getElementById(id);
+
+    if (input == null) {
+      return;
+    }
 
     if (localStorage["setting_" + id]) {
         if (input.type == "checkbox") {
@@ -117,6 +154,106 @@ function trackSetting(id) {
 
         settings[id] = localStorage["setting_" + id];
     });
+}
+
+async function updatePlaylist() {
+  var data = await fetch("/updatePlaylist/" + playlistName, { 
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ 
+      songs: songs,
+      requests: requests,
+    }),
+  });
+}
+
+function acceptRequest(index) {
+  songs.push(requests[index].songURL.slice(-11));
+  requests.splice(index, 1);
+
+  if (songs.length == 1) {
+    drawSongs();
+  }
+  drawRequests();
+
+  updatePlaylist();
+}
+
+function rejectRequest(index) {
+  console.log(index);
+  requests.splice(index, 1);
+  drawRequests();
+
+  updatePlaylist();
+}
+
+function drawRequests() {
+
+  var requestsList = document.getElementById("requests");
+
+  if (requests.length == 0) {
+    requestsList.innerHTML = "<p style='color: #7f7f7f'>No requests awaiting.</p>";
+  } else {
+    requestsList.innerHTML = "";
+    for (let i = 0; i < requests.length; i++) {
+      requestsList.innerHTML += `<hr><p id='request-${i}'>${requests[i].userName} requested <a href="${requests[i].songURL}">${requests[i].songName}</a><br/><button onclick="acceptRequest(${i})">Accept</button> <button onclick="rejectRequest(${i})">Reject</button></p>`;
+    }
+    requestsList.innerHTML += "<hr>";
+  }
+}
+
+function drawSongs() {
+
+  var content = document.getElementById("content");
+  var currentSongInfo = document.getElementById("currentSongInfo");
+
+  if (songs.length == 0) {
+    content.innerHTML = "<p style='color: #7f7f7f'>No songs in playlist.</p>";
+    currentSongInfo.style.display = "none";
+  } else if (player.videoTitle == undefined) {
+    startVideo();
+    content.innerHTML = "<p>Loading...</p>";
+    currentSongInfo.style.display = "unset";
+  } else {
+    content.innerHTML = `<p> Playing: ${player.videoTitle}</p>`;
+  }
+  var songsList = document.getElementById("songs");
+  var html = "<table style='width: 100%; table-layout: fixed'>";
+  getSongName(songs).then(names => {
+    for (let i = 0; i < songs.length; i++) {
+      html += `<tr><td style="text-align: right;"><a href="https://www.youtube.com/watch?v=${songs[i]}" style="color:white;text-decoration:none">${names[songs[i]]} </a></td><td style="text-align: left;"> <button onclick="removeVideo(${i})">Remove</button></td></tr>`;
+    }
+    html += "</table><hr>";
+
+    songsList.innerHTML = html;
+  });
+}
+
+function addSong() {
+  var url = document.getElementById("forceAddURL").value;
+
+  if (url == "") {
+    return;
+  }
+
+  songs.push(url.slice(-11));
+  drawSongs();
+  updatePlaylist();
+
+  document.getElementById("forceAddURL").value = "";
+}
+
+async function getSongName(ids) {
+  return await (await fetch("http://localhost:3000/videoTitle", {
+  headers: {
+      'Accept': 'application/json', 
+      "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ids}),
+  method: "POST"
+})).json();
 }
 
 trackSetting("shuffle");
--- index.js
+++ index.js
@@ -4,25 +4,44 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
 const fs = require("fs");
+const os = require("os");
 const app = express();
+const cookieParser = require('cookie-parser');
+const qrcode = require('qrcode');
 
-const dbKey = process.env.DB_KEY;
 const maxLength = process.env.MAX_NAME_LENGTH ?? 256;
 const PORT = process.env.PORT ?? 3000;
 
-// for parsing application/json
-app.use(bodyParser.json()); 
+const videoTitles = {};
+const playlists = {};
+const playlistNames = [];
+dbRead("playlists").then(names => {
 
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true })); 
-//form-urlencoded
+  console.log(names);
 
-// for parsing multipart/form-data
+  if (Array.isArray(names) == false) {
+    dbWrite("playlists", []);
+  }
+  
+  console.log("Loaded " + names.length + " playlists");
+  for (var i = 0; i < names.length; i++) {
+    var j = i;
+    dbRead("playlist-" + names[i]).then(data => {
+      playlists[names[j]] = data; 
+      console.log("Playlist " + names[j] + " has " + playlists[names[j]].songs.length + " songs and " + playlists[names[j]].requests.length + " requests");
+    });
+    playlistNames.push(names[i]);
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array()); 
 app.use(express.static('public'));
+app.use(cookieParser());
 
 app.get('/playlist/*', (req, res) => {
-  var name = req.path.substring(10);
+  var name = decodeURIComponent(req.path.substring(10));
 
   if (!validateName(name)) {
     res.status(400);
@@ -34,19 +53,49 @@ app.get('/playlist/*', (req, res) => {
   res.send(contents);
 });
 app.get('/request/*', (req, res) => {
-  var name = req.path.substring(10);
+  var name = decodeURIComponent(req.path.substring(9));
 
   if (!validateName(name)) {
     res.status(400);
     res.redirect("/?error=invalidName");
     return;
   }
+
+  if (playlists[name] == undefined) {
+    res.status(404);
+    res.redirect("/?error=playlistNotFound");
+    return;
+  }
   var contents = fs.readFileSync(__dirname + '/html/request.html', {encoding: 'utf-8'}).replace(/{playlistName}/g, name);
 
   res.send(contents);
 });
+app.get('/requestQR/*', (req, res) => {
+  var name = decodeURIComponent(req.path.substring(11));
+
+  if (!validateName(name)) {
+    res.status(400);
+    res.redirect("/?error=invalidName");
+    return;
+  }
+
+  if (playlists[name] == undefined) {
+    res.status(404);
+    res.redirect("/?error=playlistNotFound");
+    return;
+  }
+  
+  var contents = fs.readFileSync(__dirname + '/html/requestQR.html', {encoding: 'utf-8'}).replace(/{playlistName}/g, name);
+  var url = "https://" + req.headers.host + req.path;
+  qrcode.toDataURL(url, {scale: 20}, (err, qr) => {
+    contents = contents.replace("{qrcode}", qr);
+    contents = contents.replace("{qrcodeURL}", url);
+
+    res.send(contents);
+  });
+});
 app.get('/playlistData/*', (req, res) => {
-  var name = req.path.substring(14);
+  var name = decodeURIComponent(req.path.substring(14));
 
   if (!validateName(name)) {
     res.status(400);
@@ -54,34 +103,172 @@ app.get('/playlistData/*', (req, res) => {
     return;
   }
   
-  res.send({error: "", name: name, songs: [], requests: []});
+  res.send({error: "", name: name, songs: playlists[name].songs, requests: playlists[name].requests});
 });
+app.get("/clearName", (req, res) => {
+  res.send(__dirname + '/html/clearName.html');
+});
+app.post("/videoTitle", (req, res) => {
+  var ids = req.body.ids;
 
+  console.log(req.body);
+  console.log(videoTitles);
+
+  if (Array.isArray(ids) == false) {
+    ids = [ids];
+  }
+
+  var titles = {};
+  var promises = [];
+
+  for (var i = 0; i < ids.length; i++) {
+    var id = ids[i];
+    console.log(id);
+    if (videoTitles[id] != undefined) {
+      titles[id] = videoTitles[id];
+    } else {
+      var promise = fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBp8HnebL9IzdnEPmmfvkkt9vIhFmKc4bU&id=" + id + "&part=localizations").then(x => x.json());
+
+      
+      var j = i;
+      promises.push(
+      promise.then(x => {
+        try {
+          var id = x.items[0].id;
+          try {
+            var title = x.items[0].localizations.en.title;
+            console.log(title, id);
+            videoTitles[id] = title;
+            titles[id] = title;
+          } catch {
+            videoTitles[id] = "Untitled (API Error)";
+            titles[id] = "Untitled (API Error)";
+          }
+        } catch {
+          videoTitles[id] = "Video Not Found (API Error)";
+          titles[id] = "Video Not Found (API Error)";
+        }
+      }));
+    }
+  }
+  Promise.all(promises).then(x => {
+    res.send(titles);
+  });
+});
+app.get("/videoTitle/*", (req, res) => {
+  var name = decodeURIComponent(req.path.substring(12));
+  
+  if (videoTitles[name] != undefined) {
+    res.send(videoTitles[name]);
+    return;
+  }
+
+  console.log(name);
+
+  fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBp8HnebL9IzdnEPmmfvkkt9vIhFmKc4bU&id=" + name + "&part=localizations").then(x => x.json()).then(x => {
+    try {
+      var title = x.items[0].localizations.en.title;
+      videoTitles[name] = title;
+      res.send(title);
+    } catch {
+      videoTitles[name] = "Not Found";
+      res.send("Not Found");
+    }
+  });
+});
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/html/' + req.path);
+});
 app.post("/updatePlaylist/*", (req, res) => {
-  var name = req.path.substring(14);
+  var name = decodeURIComponent(req.path.substring(16));
+  console.log(name);
   if (!validateName(name)) {
     res.status(400);
     res.send("INVALID_NAME");
     return;
   }
+  console.log(req.cookies);
+  if ((req.cookies == undefined ? "" : req.cookies.password) != playlists[name].password) {
+    res.status(401);
+    res.send("NO_AUTHENTICATION");
+    return;
+  }
 
-  
+  var playlist = playlists[name];
+  if (playlist == undefined) {
+    res.status(404);
+    res.send("PLAYLIST_NOT_FOUND");
+    return;
+  }
+
+  playlist.songs = [...new Set(req.body.songs)];
+  playlist.requests = req.body.requests;
+  dbWrite("playlist-" + name, playlist);
 
   res.status(200);
   res.send("OK");
 });
-
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/html/' + req.path);
-});
-
-app.post("/createPlaylist", (req, res) => {
+app.post("/createPlaylist", async (req, res) => {
   var name = req.body.playlistName;
+  var password = req.body.password;
 
   if (!validateName(name)) {
     res.redirect("/?error=invalidName");
     return;
   }
+
+  console.log(playlists);
+
+  if (req.body.readonly == undefined && playlists[name] && playlists[name].password != password) {
+    res.redirect("/?error=wrongUserOrPassword");
+    return;
+  }
+
+  if (!playlists[name]) {
+    console.log("Created playlist " + name + " with password " + password);
+
+    var playlist = {name: name, password: password, songs: [], requests: []};
+    playlistNames.push(name);
+    playlists[name] = playlist;
+  
+    await dbWrite("playlist-" + name, playlist);
+    await dbWrite("playlists", playlistNames);
+  }
+
+  res.cookie("password", password);
+  
+  res.redirect("/playlist/" + name);
+});
+app.post("/request/*", (req, res) => {
+  var name = decodeURIComponent(req.path.substring(9));
+  var userName = req.body.userName;
+  var songName = req.body.songName;
+  var songURL = req.body.songURL;
+
+  if (!validateName(name)) {
+    res.redirect("/?error=invalidName");
+    return;
+  }
+
+  if (songName == undefined || songURL == undefined) {
+    res.status(400);
+    res.send("BAD_REQUEST");
+    return;
+  }
+
+  var playlist = playlists[name];
+
+  if (playlist == undefined) {
+    res.status(404);
+    res.send("PLAYLIST_NOT_FOUND");
+    return;
+  }
+
+  playlist.requests.push({userName: userName, songName: songName, songURL: songURL});
+
+  dbWrite("playlist-" + name, playlist);
+
+  console.log(userName + " added song " + songName + " to playlist " + name);
   
   res.redirect("/playlist/" + name);
 });
@@ -105,6 +292,28 @@ function validateName(name) {
     }
   }
   return true;
+}
+function automaticFilter(requester, songName, songURL) {
+
+  return true;
+}
+
+async function dbWrite(key, value) {
+  // SET userSession jsonObject
+  return (await(await fetch(`${process.env.KV_REST_API_URL}/set/${key}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+    },
+    body: JSON.stringify(value),
+    method: 'POST',
+  })).json()).result;
+}
+async function dbRead(key) {
+  return JSON.parse((await (await fetch(`${process.env.KV_REST_API_URL}/get/${key}`, {
+  headers: {
+    Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
+  },
+  })).json()).result);
 }
 
 module.exports = app;
